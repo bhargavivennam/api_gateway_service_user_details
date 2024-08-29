668a2256ccae56a96ed5423b32be49a75936cc3a
bhargavivennam <51819569+bhargavivennam@users.noreply.github.com>
2024-08-28 20:18:32 -0500

    Added api-gateway for authentication and authorization of requests

---
+++ src/main/java/com/consultingfirm/api_gateway_service/ApiGatewayConfig.java
@@ -0,0 +1,21 @@
+package com.consultingfirm.api_gateway_service;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.GatewayFilterSpec;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApiGatewayConfig {
+
+    @Bean
+    public RouteLocator gatewayRouter(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(p -> p.path("/api/users/**", "/swagger-ui/**","/v3/api-docs/**")
+                        .filters(GatewayFilterSpec::tokenRelay)
+                        .uri("lb://user-details"))
+//                        .uri("http://localhost:9090")) //user details microservice port number
+                .build();
+    }
+}
